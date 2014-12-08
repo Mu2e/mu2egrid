435d60450ce967652bea747b2a547d405252c584
Andrei Gaponenko <gandr@fnal.gov>
2014-12-08 14:38:27 -0600

    mu2eart: factor out common options into the pm file.

--- bin/mu2egrid.pm
+++ bin/mu2egrid.pm
@@ -11,7 +11,9 @@ use strict;
 use Cwd 'abs_path';
 use File::Basename;
 
-our $jobsub = 'jobsub_submit';
+our $impldir;
+$impldir = abs_path(dirname($0) . '/impl');
+
 our @knownOutstage = ('/mu2e/data/outstage',
 		      '/mu2e/data2/outstage',
 		      '/grid/data/mu2e/outstage',
@@ -21,8 +23,130 @@ our @knownOutstage = ('/mu2e/data/outstage',
 
 our $mu2eDefaultOutstage = $knownOutstage[0];
 
-our $impldir;
-$impldir = abs_path(dirname($0) . '/impl');
+#================================================================
+sub default_group_helper() {
+    my @res;
+    if(defined($ENV{'GROUP'})) {
+	@res = ('group' => $ENV{'GROUP'});
+    }
+    return @res;
+}
+
+our $jobsub = 'jobsub_submit';
+
+our @commonOptList = (
+		      'group=s',
+		      'role=s',
+		      'jobsub-server=s',
+		      'disk=i',
+		      'memory=i',
+		      'OS=s',
+		      'resource-provides=s',
+		      'jobsub-arg=s@',
+		      'ifdh-version=s',
+		      'outstage=s',
+		      "prestage-spec=s",
+		      'test-queue',
+		      'dry-run',
+		      'verbose',
+		      'help',
+		      );
+
+# those that are not defaulted must be tested with exists($opt{'option'}) before accessing their values
+our %commonOptDefaults = (
+			  default_group_helper(),
+			  'jobsub-server' => 'https://fifebatch.fnal.gov:8443',
+			  'disk' => '30000', # MB
+			  'memory' => '2048', # MB
+			  'OS' => 'SL5,SL6',
+			  'resource-provides' => 'usage_model=OPPORTUNISTIC,DEDICATED',
+			  'outstage' => $mu2egrid::mu2eDefaultOutstage,
+			  'test-queue' => 0,
+			  'dry-run' => 0,
+			  'verbose' => 0,
+			  'help' => 0,
+			  );
+
+sub commonOptDoc1() {
+    return <<EOF
+	      [--group=<name>] \\
+	      [--role=<name>] \\
+	      [--jobsub-server=<URL>] \\
+	      [--disk=<size_MB>] \\
+	      [--memory=<size_MB>] \\
+	      [--OS=<comma_separated_list>] \\
+	      [--resource-provides=<spec>] \\
+	      [--jobsub-arg=string1] [--jobsub-arg=string2] [...] \\
+	      [--ifdh-version=<version>] \\
+	      [--outstage=<dir>] \\
+	      [--prestage-spec=<file>] \\
+	      [--test-queue] \\
+	      [--dry-run] \\
+	      [--verbose] \\
+	      [--help]
+EOF
+;
+}
+
+sub commonOptDoc2() {
+    return <<EOF
+    - The --group, --role, --jobsub-server, --disk, --memory, --OS,
+      and --resource-provides options are passed to jobsub_submit.
+      Arbitrary other jobsub_submit options can be passed using
+      --jobsub-arg.  Their default values are
+
+          --group              from the GROUP environment variable
+	  --role               none
+	  --jobsub-server      $commonOptDefaults{'jobsub-server'}
+	  --disk               $commonOptDefaults{'disk'}
+	  --memory             $commonOptDefaults{'memory'}
+          --OS                 $commonOptDefaults{'OS'}
+          --resource-provides  $commonOptDefaults{'resource-provides'}
+
+    --ifdh-version=<version> exports the requested IFDH_VERSION to the
+      worker node, where it is supposed to affect jobsub wrapper
+      scripts.
+
+    - Outstage should be one of the following registered locations:
+
+           @mu2egrid::knownOutstage
+
+      by default $mu2egrid::mu2eDefaultOutstage  is used.
+
+    - The --prestage-spec option allows to specify a list of extra
+      files that should be prestaged to the worker node.  Each
+      line in the specification file has the format:
+
+      /file/name/on/bluarc   relative/file/name/on/worker/node
+
+      that is, source and target file names separated by any amount of
+      white space.  The target file name is relative to the working
+      directory.  It must contain a slash '/' and must not start with
+      a slash. Leading and trailing white spaces are ignored.
+
+    - The --test-queue option submits a test job.  The job will run
+      with highest possible priority, but you can only have one such
+      job in the queue at a time.
+
+    - Use --dry-run to test the submission command without actually
+      sending the jobs.  Usually used in conjunction with --verbose.
+
+    - Add --verbose if you want to see the details of what is going on.
+
+    - The --help option prints this message.
+
+Once grid jobs are submitted the software libraries pointed to by the
+setup script, all configuration files, and the input file list, must
+be left intact until all the jobs finish.
+
+The square brackets [] above denote optional settings, and
+{alternative1|alternative2} constructs denote mutually exclusive
+alternatives.  All option names may be abbreviates as long as this is
+unambiguous.  (For example, '--verbose' and '--verb' mean the same
+thing.)
+EOF
+;
+}
 
 #================================================================
 sub assert_known_outstage($) {
@@ -86,22 +210,22 @@ sub validate_prestage_spec($) {
 BEGIN {
     use Exporter   ();
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
-    
+
     # set the version for version checking
     $VERSION = '1.01';
 
     @ISA         = qw(Exporter);
-    @EXPORT      = qw( &find_file &validate_file_list &validate_prestage_spec &assert_known_outstage $impldir
-                      );
-
-    %EXPORT_TAGS = ( all => [qw( &find_file &validate_file_list &validate_prestage_spec &assert_known_outstage
-				 $jobsub $impldir @knownOutstage $mu2eDefaultOutstage
-                                 )] 
-                     );
+    @EXPORT      = qw();
+    ## %EXPORT_TAGS = ( all => [qw( &find_file $impldir @knownOutstage $mu2eDefaultOutstage )] );
 
     # your exported package globals go here,
     # as well as any optionally exported functions
-    @EXPORT_OK   = qw( $jobsub $impldir @knownOutstage $mu2eDefaultOutstage );
+    @EXPORT_OK   = qw(
+		      $impldir
+		      @knownOutstage $mu2eDefaultOutstage
+		      $jobsub @commonOptList %commonOptDefaults &commonOptDoc1 &commonOptDoc2
+		      &assert_known_outstage &find_file &validate_file_list &validate_prestage_spec
+		      );
 }
 our @EXPORT_OK;
 use vars @EXPORT_OK;
