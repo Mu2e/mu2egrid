81833d90ff3b97805f677df8f973c5901f7ab977
Andrei Gaponenko <gandr@fnal.gov>
2016-07-11 20:13:02 -0500

    Fix a diagnostic message.

--- bin/impl/mu2eprodsys.sh
+++ bin/impl/mu2eprodsys.sh
@@ -316,7 +316,7 @@ clustername="$cluster${MU2EGRID_CLUSTERNAME:+.$MU2EGRID_CLUSTERNAME}"
 jobname=failedjob
 export logFileName="${jobname}.log"
 declare -a outfiles=( $logFileName )
-finalOutDir="${MU2EGRID_WFOUTSTAGE:?Error: MU2EGRID_OUTSTAGE is not set}/$clustername/$(printf %02d $((${PROCESS:-0}/1000)))/$(printf %05d ${PROCESS:-0})"
+finalOutDir="${MU2EGRID_WFOUTSTAGE:?Error: MU2EGRID_WFOUTSTAGE is not set}/$clustername/$(printf %02d $((${PROCESS:-0}/1000)))/$(printf %05d ${PROCESS:-0})"
 
 #================================================================
 # Set up Mu2e environment and make ifdh available
