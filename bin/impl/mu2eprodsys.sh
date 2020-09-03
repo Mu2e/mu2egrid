Mu2e/mu2egrid

commit b5926ccc2b4a7898521b52ed66b9d924d7b9f289

Move JSON generation to the mu2efilename package.

bin/impl/mu2eprodsys.sh
@@ -84,197 +84,6 @@ filterOutProxy() {
     done
 }
 #================================================================
-printJson() {
-    # Because this JSON generation code is meant for grid running,
-    # some conditions that otherwise would have been treated as errors
-    # are given a pass.  For example, if an unknown data_tier is
-    # encountered, the file_type is simply set to "other".  When a new
-    # data_tier is defined and this code is not updated before job
-    # submission, it is easier to manually fix json after jobs succeed
-    # than to re-run all the jobs.  And one can still use the outputs
-    # even if they are not registered in SAM.
-
-    # We saw DB performance issues when writing very long subrun lists to SAM.
-    # The compromise is to drop subrun list from MC metadata if it is too long.
-    maxLengthOfMCSubrunList=100
-
-    printJsonError() {
-        echo "Usage:"
-        echo "    $0 {--parents parentListFile|--no-parents} validMu2eFileName"
-    } >&2
-
-    case "$1" in
-        --parents)
-            parentfile="$2";
-            if [[ ! -r $parentfile ]]; then
-                echo "Error: parent file \"$parentfile\" is not readable." >&2
-                exit 2
-            fi
-            shift
-            ;;
-
-        --no-parents) parentfile="";;
-        *) printJsonError ; exit 1 ;;
-    esac
-    shift
-
-    if [[ $# != 1 ]]; then
-        printJsonError
-        exit 1
-    fi
-
-    #================================================================
-    inputfile=$1
-    filename=$(basename $inputfile)
-
-    fnfields=($(echo $filename | sed -e 's/\./ /g'))
-
-    if [[ ${#fnfields[@]} != 6 ]]; then
-        echo "Error: the \"$filename\" does not look like a valid Mu2e file name: expect 6 dot separated fields, got ${#fnfields[@]}" >& 2
-        exit 2
-    fi
-
-    ### The first block of metadata is directly read from the filename
-    cat<<EOF
-{
-    "file_name": "$filename",
-    "dh.dataset": "${fnfields[0]}.${fnfields[1]}.${fnfields[2]}.${fnfields[3]}.${fnfields[5]}",
-    "data_tier": "${fnfields[0]}",
-    "dh.owner": "${fnfields[1]}",
-    "dh.description": "${fnfields[2]}",
-    "dh.configuration": "${fnfields[3]}",
-    "dh.sequencer": "${fnfields[4]}",
-    "file_format": "${fnfields[5]}",
-EOF
-
-    # The file_type parameter is derived from the filename using the
-    # mappings.  We do not want to crash grid jobs here
-    tier="${fnfields[0]}"
-    case "$tier" in
-        raw|rec|ntd|ext|rex|xnt) filetype=data ;;
-        cnf|sim|mix|dig|mcs|nts) filetype=mc ;;
-        *) filetype=other;;
-    esac
-
-    cat<<EOF
-    "file_type": "$filetype",
-EOF
-
-    # The following piece is queried from the filesystem
-    cat<<EOF
-    "file_size": "$(stat -c '%s' $inputfile)",
-EOF
-
-    # The hash of the file content
-    cat<<EOF
-    "dh.sha256": "$(sha256sum $inputfile|cut -f1 -d' ')",
-EOF
-
-    # A list of parents, if provided
-    if [[ -n "$parentfile" ]]; then
-        sep=" "
-        echo  '    "parents": ['
-        (while read line; do
-            echo '      '$sep' "'$line'"'; sep=,
-            done ) < "$parentfile"
-        echo '    ],'
-    fi
-
-    # For art files only, run/subrun/event info determined from file content.
-    ext="${fnfields[5]}"
-    if [[ $ext == art ]]; then
-        if which file_info_dumper > /dev/null ; then
-
-            file_info_dumper --event-list "$inputfile" \
-                | awk '
-BEGIN{
-  started=0;
-  eventCount=0;
-  subrunCount=0;
-  fer="";
-  fsr="";
-};
-{
-  if(started) {
-    if(NF==2) {
-      # process subrun list
-      ++subrunCount;
-      if(!fsr) fsr=$0;
-      lsr=$0;
-
-      if((subrunCount < 1+'$maxLengthOfMCSubrunList') || ("'$filetype'" != "mc")) {
-          asr[subrunCount] = $0;
-      }
-      else {
-          # Empty the subrun list
-          for(i in asr) delete asr[i];
-      }
-
-    }
-    if(NF==3) {
-      # process event list
-      ++eventCount;
-      if(!fer) fer=$0;
-      ler=$0;
-    }
-  };
-};
-/Run *SubRun *Event/{started=1};
-END {
-  print "    \"event_count\": "eventCount",";
-
-# Info on the first (sorted) event in the file
-  split(fer, fera);
-  print "    \"dh.first_run_event\": "fera[1]",";
-  print "    \"dh.first_subrun_event\": "fera[2]",";
-  print "    \"dh.first_event\": "fera[3]",";
-
-# Info on the first (sorted) event in the file
-  split(ler, lera);
-  print "    \"dh.last_run_event\": "lera[1]",";
-  print "    \"dh.last_subrun_event\": "lera[2]",";
-  print "    \"dh.last_event\": "lera[3]",";
-
-# Info on the first (sorted) subrun in the file
-  split(fsr, fsra);
-  print "    \"dh.first_run_subrun\": "fsra[1]",";
-  print "    \"dh.first_subrun\": "fsra[2]",";
-
-# Info on the last (sorted) subrun in the file
-  split(lsr, lsra);
-  print "    \"dh.last_run_subrun\": "lsra[1]",";
-  print "    \"dh.last_subrun\": "lsra[2]",";
-
-# List of subruns
-  print "    \"runs\": [";
-  sep=" ";
-  for(i in asr) {
-    split(asr[i],tmp);
-    print "      " sep " [ " tmp[1] ", " tmp[2] ", \"'$filetype'\" ]";
-    sep=",";
-  }
-  print "    ],";
-
-}
-'
-        else
-            cat >&2 <<EOF
-Error: no file_info_dumper in PATH.
-It is needed to extract metadata from art files.
-Art not setup or its version is not compatible.
-EOF
-            exit 3
-        fi
-    fi
-
-    # The fixed ending.
-    cat<<EOF
-    "content_status": "good"
-}
-EOF
-} # printJson() end
-
-#================================================================
 # Run the framework jobs and create json files for the outputs
 # Running it inside a function makes it easier to exit on error
 # during the "payload" part, but still transfer the log file back.
@@ -352,6 +161,8 @@ mu2eprodsys_payload() {
             echo "MU2EGRID_MU2EBINTOOLS_VERSION not defined - will setup current mu2etools"
             setup mu2etools
         fi
+
+        setup -B mu2efilename "${MU2EGRID_MU2EFILENAME_VERSION}"
 
         echo "#================================================================"
         echo "# mu2eprodsys $(date) -- $(date +%s) After package setup, the environment is:"
@@ -542,7 +353,7 @@ mu2eprodsys_payload() {
         shopt -s nullglob
 
         for i in *.art *.root; do
-            printJson --parents parents $i > $i.json
+            printJson.sh --parents parents $i > $i.json
         done
 
         rm -f parents

bin/mu2eprodsys
@@ -59,6 +59,7 @@ Usage:
               [--wfproject=<name>] \\
               [--clustername=<name>] \\
               [--mu2ebintools-version=<string>] \\
+              [--mu2efilename-version=<string>] \\
               [--xrootd] \\
               [--transfer-all-files] \\
               [--error-delay=<int>] \\
@@ -121,7 +122,16 @@ EOF
       version of the corresponding package to be used by the worker
       node process.  If a version is not specified on the command
       line, mu2eprodsys will rely on the mu2etools package to find a
-      compatible mu2ebintools version.
+      compatible mu2ebintools version. The version will be fixed at
+      the job submission time, unless the --code options is used.
+      With the --code options the package version resolution is
+      postponed until the process starts on the worker node, which
+      might lead to different jobs from the same submission using
+      different mu2ebintools versions.
+
+    - The --mu2efilename-version option can be used to override
+      the default package version.  The version is fixed at the
+      job submission time.
 
     - The --xrootd option alters the way input art files are accessed
       by the worker node process.  By default all data files specified
@@ -205,6 +215,7 @@ GetOptions(\%opt,
     'wfproject=s',
     'clustername=s',
     'mu2ebintools-version=s',
+    'mu2efilename-version=s',
     'error-delay=i',
     'xrootd',
     'transfer-all-files',
@@ -310,6 +321,9 @@ else {
     print "WARNING: Can not determine mu2ebintools version at submission time when --code is used.\n";
 }
 
+$ENV{'MU2EGRID_MU2EFILENAME_VERSION'} = $opt{'mu2efilename-version'} // mu2egrid::default_package_version($opt{'mu2e-setup'}, 'mu2efilename', '');
+print "Will use mu2efilename version $ENV{MU2EGRID_MU2EFILENAME_VERSION}\n";
+
 $ENV{'MU2EGRID_ERRORDELAY'} = $opt{'error-delay'} // ($njobs > 10 ?  $default_error_delay : 1);
 
 $ENV{'MU2EGRID_IFDHEXTRAOPTS'} = $opt{'ifdh-options'} // '';
@@ -322,7 +336,7 @@ while( (my $key, my $value) = each(%ENV) ) {
             push @args,  ('-e', $key);
             print "Using: $key => $ENV{$key}\n";
         }
-}
+    }
 }
 
 push @args, ('-N', $njobs);

commit 3e055b27a03097e23c039e0fabc49548403ae75d

mu2ehpc was broken by commit b5926ccc, fix that.

bin/impl/mu2eprodsys.sh
@@ -293,7 +293,13 @@ mu2eprodsys_payload() {
             setup mu2etools
         fi
 
-        setup -B mu2efilename "${MU2EGRID_MU2EFILENAME_VERSION}"
+        if [[ $MU2EGRID_HPC ]]; then
+            # Package version inside container can not be determined at submission time.
+            # We use the container's "current" here, but the container itself is versioned.
+            setup mu2efilename
+        else
+            setup -B mu2efilename "${MU2EGRID_MU2EFILENAME_VERSION}"
+        fi
 
         echo "#================================================================"
         echo "# mu2eprodsys $(date) -- $(date +%s) After package setup, the environment is:"